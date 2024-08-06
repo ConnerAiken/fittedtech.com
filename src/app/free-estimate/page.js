21e70dc026c43e9480a50e0287df3f503d34630e
ConnerAiken <conner@fittedtech.com>
2024-08-05 19:32:56 -0700

    Connect the free estimate tab

---
+++ src/app/free-estimate/page.js
@@ -0,0 +1,18 @@
+export default function Contact() {
+  return (
+    <section className="bg-gray-50 dark:bg-gray-800">
+      <div className="max-w-screen-xl px-4 py-8 mx-auto space-y-12 lg:space-y-20 lg:py-24 lg:px-6">
+        {/* Row */}
+        <div className="items-center mt-16">
+          <div class="mb-8">
+            <h1 className="max-w-2xl mb-4 text-4xl font-extrabold leading-none tracking-tight md:text-5xl xl:text-6xl dark:text-white">Services</h1>
+            <p className="text-gray-500 sm:text-lg dark:text-gray-400">
+              We offer a variety of services to meet your needs. Whether you need help with IT services or website/application development.
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
