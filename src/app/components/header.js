0d5cb3920795cc425b26ab36078421a060a32610
ConnerAiken <conner@fittedtech.com>
2024-08-05 19:33:47 -0700

    Route the logo to homepage

--- src/app/components/header.js
+++ src/app/components/header.js
@@ -18,7 +18,7 @@ export const Header = () => {
     <header className="fixed w-full">
       <nav className="bg-white border-gray-200 py-2.5 dark:bg-gray-900">
         <div className="flex flex-wrap items-center justify-between max-w-screen-xl px-4 mx-auto">
-          <a href="#" className="flex items-center">
+          <a href="/" className="flex items-center">
             <Image src={"logos/logo-dark-cropped.svg"} alt="logo" width={200} height={75} />
           </a>
           <div className="flex items-center lg:order-2">
